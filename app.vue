f94aaf5ea0bc36bdb5ce2a5e1712293ecf8591ef
krutoi2006 <pustovoit116@gmail.com>
2026-07-26 19:54:24 +0300

    metrika

--- app.vue
+++ app.vue
@@ -1,3 +1,39 @@
+<script setup lang="ts">
+useHead({
+  script: [
+    {
+      id: 'yandex-metrika',
+      innerHTML: `(function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a);
+      })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js?id=110976397', 'ym');
+
+      ym(110976397, 'init', {
+        ssr: true,
+        webvisor: true,
+        clickmap: true,
+        ecommerce: 'dataLayer',
+        referrer: document.referrer,
+        url: location.href,
+        accurateTrackBounce: true,
+        trackLinks: true
+      });`,
+    },
+  ],
+});
+</script>
+
 <template>
   <NuxtPage />
+  <noscript>
+    <div>
+      <img
+        src="https://mc.yandex.ru/watch/110976397"
+        style="position: absolute; left: -9999px"
+        alt=""
+      />
+    </div>
+  </noscript>
 </template>
